c754bf8c9a909387041120b6164a1e675653714f
anoveand <38746389+anoveand@users.noreply.github.com>
2018-04-28 06:27:35 +0700

    Add files via upload

---
+++ J3C117076_Havel A_Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.r = New System.Windows.Forms.Label()
+        Me.h = New System.Windows.Forms.Label()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.n1 = New System.Windows.Forms.TextBox()
+        Me.n2 = New System.Windows.Forms.TextBox()
+        Me.n3 = New System.Windows.Forms.TextBox()
+        Me.hitung = New System.Windows.Forms.Button()
+        Me.ulang = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.nextt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(49, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(49, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nilai 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(49, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nilai 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(49, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nilai 3"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(49, 166)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Rataan"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(49, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Hadiah"
+        '
+        'r
+        '
+        Me.r.AutoSize = True
+        Me.r.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.r.Location = New System.Drawing.Point(150, 166)
+        Me.r.Name = "r"
+        Me.r.Size = New System.Drawing.Size(141, 20)
+        Me.r.TabIndex = 6
+        Me.r.Text = "................................."
+        '
+        'h
+        '
+        Me.h.AutoSize = True
+        Me.h.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.h.Location = New System.Drawing.Point(150, 202)
+        Me.h.Name = "h"
+        Me.h.Size = New System.Drawing.Size(141, 20)
+        Me.h.TabIndex = 7
+        Me.h.Text = "................................."
+        '
+        'txtnama
+        '
+        Me.txtnama.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtnama.Location = New System.Drawing.Point(153, 30)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(238, 26)
+        Me.txtnama.TabIndex = 8
+        '
+        'n1
+        '
+        Me.n1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n1.Location = New System.Drawing.Point(153, 60)
+        Me.n1.Name = "n1"
+        Me.n1.Size = New System.Drawing.Size(100, 26)
+        Me.n1.TabIndex = 9
+        '
+        'n2
+        '
+        Me.n2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n2.Location = New System.Drawing.Point(153, 90)
+        Me.n2.Name = "n2"
+        Me.n2.Size = New System.Drawing.Size(100, 26)
+        Me.n2.TabIndex = 10
+        '
+        'n3
+        '
+        Me.n3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n3.Location = New System.Drawing.Point(153, 124)
+        Me.n3.Name = "n3"
+        Me.n3.Size = New System.Drawing.Size(100, 26)
+        Me.n3.TabIndex = 11
+        '
+        'hitung
+        '
+        Me.hitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hitung.Location = New System.Drawing.Point(297, 163)
+        Me.hitung.Name = "hitung"
+        Me.hitung.Size = New System.Drawing.Size(94, 28)
+        Me.hitung.TabIndex = 13
+        Me.hitung.Text = "Hitung"
+        Me.hitung.UseVisualStyleBackColor = True
+        '
+        'ulang
+        '
+        Me.ulang.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ulang.Location = New System.Drawing.Point(297, 197)
+        Me.ulang.Name = "ulang"
+        Me.ulang.Size = New System.Drawing.Size(94, 30)
+        Me.ulang.TabIndex = 14
+        Me.ulang.Text = "Ulang"
+        Me.ulang.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Items.AddRange(New Object() {"Jika rataan > 80, hadiah Rp 10 juta.", "Jika rataan > 70, hadiah Rp 7,5 juta.", "Selainnya hanya dapat sertifikat."})
+        Me.ListBox1.Location = New System.Drawing.Point(52, 233)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(339, 84)
+        Me.ListBox1.TabIndex = 16
+        '
+        'nextt
+        '
+        Me.nextt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nextt.Location = New System.Drawing.Point(297, 84)
+        Me.nextt.Name = "nextt"
+        Me.nextt.Size = New System.Drawing.Size(94, 32)
+        Me.nextt.TabIndex = 17
+        Me.nextt.Text = "Next"
+        Me.nextt.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 345)
+        Me.Controls.Add(Me.nextt)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ulang)
+        Me.Controls.Add(Me.hitung)
+        Me.Controls.Add(Me.n3)
+        Me.Controls.Add(Me.n2)
+        Me.Controls.Add(Me.n1)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.h)
+        Me.Controls.Add(Me.r)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents r As System.Windows.Forms.Label
+    Friend WithEvents h As System.Windows.Forms.Label
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents n1 As System.Windows.Forms.TextBox
+    Friend WithEvents n2 As System.Windows.Forms.TextBox
+    Friend WithEvents n3 As System.Windows.Forms.TextBox
+    Friend WithEvents hitung As System.Windows.Forms.Button
+    Friend WithEvents ulang As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents nextt As System.Windows.Forms.Button
+
+End Class
